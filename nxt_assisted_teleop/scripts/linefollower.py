8f1803a7484410b7c4fde08e56a3f63917ada209
ipa-fmw-ms <max-sieber@gmx.de>
2013-07-20 05:55:11 +0200

    attemp linefollower. Py good, C++ not working

---
+++ nxt_assisted_teleop/scripts/linefollower.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import roslib; roslib.load_manifest('nxt_assisted_teleop')  
+import rospy
+from nxt_msgs.msg import Color
+from geometry_msgs.msg import Twist
+
+
+class Follow:
+    def __init__(self):
+        self.sub = rospy.Subscriber('intensity', Color, self.sub_cb)
+        self.pub = rospy.Publisher('cmd_vel', Twist)
+
+    def sub_cb(self, msg):
+        vel = Twist()
+        #tests just speed with intensity. performance seems good
+        vel.linear.x = 0.001*(msg.intensity-227)
+        self.pub.publish(vel)
+
+def main():
+    rospy.init_node('linefollower')
+    follow = Follow()
+    rospy.spin()
+
+
+
+if __name__ == '__main__':
+    main()
