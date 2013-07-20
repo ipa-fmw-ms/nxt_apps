8518c50f2dd978dd4643100da958fbb529956591
ipa-fmw-ms <max-sieber@gmx.de>
2013-07-20 16:58:52 +0200

    shutdown and angulardiff added

--- nxt_assisted_teleop/scripts/linefollower.py
+++ nxt_assisted_teleop/scripts/linefollower.py
@@ -10,16 +10,25 @@ class Follow:
     def __init__(self):
         self.sub = rospy.Subscriber('intensity', Color, self.sub_cb)
         self.pub = rospy.Publisher('cmd_vel', Twist)
+        rospy.on_shutdown(self.shutdown)
 
     def sub_cb(self, msg):
         vel = Twist()
         #tests just speed with intensity. performance seems good
-        vel.linear.x = 0.001*(msg.intensity-227)
+        vel.linear.x = 0.0007*(msg.intensity-227)
+        vel.angular.z = 0.0007*(msg.intensity-227)
+        self.pub.publish(vel)
+
+    def shutdown(self):
+        vel = Twist()
+        vel.linear.x = 0
+        vel.angular.z = 0
         self.pub.publish(vel)
 
 def main():
     rospy.init_node('linefollower')
     follow = Follow()
+    
     rospy.spin()
 
 
